ea3044d3899f1243de0543a91a213f946191f09c
Casey Cook <cook.caseya@gmail.com>
2020-07-13 15:08:20 -0400

    initial db schemas added to models folder

---
+++ models/employee.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; 
+
+const perfAreaSchema = new Schema({
+  area1 : {
+    type: String,
+    required: true,
+    unique: true
+  },
+  area2 : {
+    type: String,
+    required: true,
+    unique: true
+  },
+  area3 : {
+    type: String,
+    required: true,
+    unique: true
+  }
+},
+{
+  timestamps: true
+})
+
+const scoreSchema = new Schema({
+  score1: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  score2: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  score3: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  }
+})
+
+const dateSchema = new Schema({
+  year: {
+    type: String,
+    required: true
+  },
+  month: {
+    type: String,
+    required: true
+  },
+  day: {
+    type: String,
+    required: true
+  }
+})
+
+const reviewsSchema = new Schema({
+  employeeId: {
+    type: Number,
+    required: true
+  },
+  id: {
+    type: Number,
+    required: true
+  },
+  reviewPage: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  date: [dateSchema],
+  areas: [perfAreaSchema],
+  scores: [scoreSchema],
+  note: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    required: true
+  }
+
+})
+
+const employeeSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  photo: {
+    type: String,
+    required: true
+  },
+  performanceAreas: [perfAreaSchema],
+  reviews: [reviewsSchema]
+},
+{
+  timestamps: true
+})
+
+
+const Employees = mongoose.model('Employees', employeesSchema);
+
+module.exports = Employees;
--- routes/employeesRouter.js
+++ routes/employeesRouter.js
@@ -1,118 +1,125 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const Employee = require('../models/employee');
 
 const employeesRouter = express.Router();
 
 employeesRouter.use(bodyParser.json());
 
 //EMPLOYEES
-employeesRouter.route('/')
+employeesRouter
+	.route('/')
 
-  .all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next(); 
-  }) 
-  
-  .get((req, res) => {
-    res.end('Will send entire EMPLOYEES array to you')
-  })
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
 
-  .post((req, res) => {
-    res.end(`Adding employee ${req.body.name} to EMPLOYEES array`)
-  })
+	.get((req, res) => {
+		res.end('Will send entire EMPLOYEES array to you');
+	})
 
-  .put((req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /employees');
-  })
+	.post((req, res) => {
+		res.end(`Adding employee ${req.body.name} to EMPLOYEES array`);
+	})
 
-  .delete((req, res) => {
-    res.statusCode = 403;
-    res.end('DELETE operation not supported on /employees')
-  });
+	.put((req, res) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on /employees');
+	})
+
+	.delete((req, res) => {
+		res.statusCode = 403;
+		res.end('DELETE operation not supported on /employees');
+	});
 
 //EMPLOYEE w/ ID
-employeesRouter.route('/:employeeId')
+employeesRouter
+	.route('/:employeeId')
 
-.all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next(); 
-}) 
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
 
-.get((req, res) => {
-  res.end(`Will send all info for employee ${req.body.employeeId} to you`)
-})
+	.get((req, res) => {
+		res.end(`Will send all info for employee ${req.body.employeeId} to you`);
+	})
 
-.post((req, res) => {
-  res.statusCode = 403;
-  res.end(`POST operation not supported on employees/employeeId`)
-})
+	.post((req, res) => {
+		res.statusCode = 403;
+		res.end(`POST operation not supported on employees/employeeId`);
+	})
 
-.put((req, res) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on employees/employeeId');
-})
+	.put((req, res) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on employees/employeeId');
+	})
 
-.delete((req, res) => {
-  res.end(`Deleting employee with id: ${req.body.employeeId}  now...`)
-});
-
+	.delete((req, res) => {
+		res.end(`Deleting employee with id: ${req.body.employeeId} now...`);
+	});
 
 //EMPLOYEE w/ ID REVIEWS
-employeesRouter.route('/:employeeId/reviews')
+employeesRouter
+	.route('/:employeeId/reviews')
 
-.all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next(); 
-}) 
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
 
-.get((req, res) => {
-  res.end(`Will send all reviews for employee ${req.body.employeeId} to you`)
-})
+	.get((req, res) => {
+		res.end(`Will send all reviews for employee ${req.body.employeeId} to you`);
+	})
 
-.post((req, res) => {
-  res.end(`Adding a review to employee reviews array`)
-})
+	.post((req, res) => {
+		res.end(`Adding a review to employee reviews array`);
+	})
 
-.put((req, res) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on employees/employeeId/reviews');
-})
+	.put((req, res) => {
+		res.statusCode = 403;
+		res.end('PUT operation not supported on employees/employeeId/reviews');
+	})
 
-.delete((req, res) => {
-  res.statusCode = 403;
-  res.end('DELETE operation not supported on employees/employeeId/reviews')
-});
+	.delete((req, res) => {
+		res.statusCode = 403;
+		res.end('DELETE operation not supported on employees/employeeId/reviews');
+	});
 
 //EMPLOYEE w/ ID /REVIEWS /:ReviewId
-employeesRouter.route('/:employeeId/reviews/:reviewId')
+employeesRouter
+	.route('/:employeeId/reviews/:reviewId')
 
-.all((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next(); 
-}) 
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
 
-.get((req, res) => {
-  res.end(`Will send review details for review id: ${req.body.reviewId} to you`)
-})
+	.get((req, res) => {
+		res.end(
+			`Will send review details for review id: ${req.body.reviewId} to you`
+		);
+	})
 
-.post((req, res) => {
-  res.end(`POST operation not supported on employees/employeeId/reviews/:reviewId`)
-})
+	.post((req, res) => {
+		res.end(
+			`POST operation not supported on employees/employeeId/reviews/:reviewId`
+		);
+	})
 
-.put((req, res) => {
-  res.statusCode = 403;
-  res.end(`completing review with id: ${req.params.reviewId} now`);
-})
+	.put((req, res) => {
+		res.statusCode = 403;
+		res.end(`Updating review with id: ${req.params.reviewId} now`);
+	})
 
-.delete((req, res) => {
-  res.statusCode = 403;
-  res.end(`Deleting review with id: ${req.body.reviewId} now`)
-});
-
+	.delete((req, res) => {
+		res.statusCode = 403;
+		res.end(`Deleting review with id: ${req.body.reviewId} now`);
+	});
 
 module.exports = employeesRouter;
